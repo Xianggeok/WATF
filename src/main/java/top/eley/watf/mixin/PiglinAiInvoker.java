c0b4bde20329e314a8179f0ad7389fead2469c58
XiangGe <ovo@eley.top>
2026-06-02 11:49:48 +0800

    fix: PiglinAi.setAngerTarget 在 1.21.1 只有 2 个参数
    - 1.21.1: setAngerTarget(AbstractPiglin, LivingEntity) - 2 参数
    - 26.x: setAngerTarget(ServerLevel, AbstractPiglin, LivingEntity) - 3 参数
    - @Invoker 改为 2 参数版本，兼容 1.21.1

--- src/main/java/top/eley/watf/mixin/PiglinAiInvoker.java
+++ src/main/java/top/eley/watf/mixin/PiglinAiInvoker.java
@@ -1,6 +1,5 @@
 package top.eley.watf.mixin;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
@@ -10,7 +9,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(PiglinAi.class)
 public interface PiglinAiInvoker {
     @Invoker("setAngerTarget")
-    static void invokeSetAngerTarget(ServerLevel level, AbstractPiglin piglin, LivingEntity target) {
+    static void invokeSetAngerTarget(AbstractPiglin piglin, LivingEntity target) {
         throw new AssertionError();
     }
 }
